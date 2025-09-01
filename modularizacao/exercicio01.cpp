325be857aeef6a7a38d162b8af2e36e2a35841af
lipeVar <167947564+lipeVar@users.noreply.github.com>
2025-09-01 18:16:58 +0000

    exercicio para casa

---
+++ modularizacao/exercicio01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+#include "metodos01.h"
+
+using namespace std;
+
+
+int main() {
+    std::string palavra;
+    char letra;
+
+    //metodo para adicionar as variaveis
+    lerPalavra (palavra, letra);
+    
+}
+    
---
+++ modularizacao/metodos01.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void lerPalavra(std::string palavra, char letra) {
+    cout << "Digite a palavra";
+    cin >> palavra;
+
+    cout << "Digite uma letra";
+    cin >> letra;
+
+    cout << "a palavra" << palavra << "tem a letra " << letra;
+}
+   
