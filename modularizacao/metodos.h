3e5d9d33e43e477de555a7d1fdaab1e22504f67b
lipeVar <167947564+lipeVar@users.noreply.github.com>
2025-09-04 11:25:54 +0000

    exercicio aula

--- modularizacao/metodos.h
+++ modularizacao/metodos.h
@@ -40,7 +40,60 @@ void menorElemento(int vetor[], int quantidade, int tamanho) {
             menor = vetor[i];
         }
     }
+
+    cout << "Menor elemento.... " << menor << endl;
+
 }
-void ordenar(int vetor[], int quantidade, int tamanho) {
+
+void escreverSeValida(int dia, int mes, int ano) {
+    //meses com 31: 1, 3, 5, 7, 8, 10, 12
+    //meses com 30: 2, 4, 6, 9, 11
+
+    if (dia == 31 && (mes == 2 || mes == 4 || mes == 6 || mes == 9 || mes == 11)) {
+        cout << "Data invalida" << endl;
+    } else if (dia == 29 && mes == 2 && (ano % 4 != 0)) {
+        cout << "Data invalida" << endl;
+    } else if (dia > 29 && mes == 2) {
+        cout << "Data invalida" << endl;
+    } else {
+        cout << "Data valida" << endl;
+    }
+}
+
+void escreverSeValida2(string data) {
+    //meses com 31: 1, 3, 5, 7, 8, 10, 12
+    //meses com 30: 2, 4, 6, 9, 11
+
+    //data = 01/09/2025
+    int dia = stoi(data.substr(0,2));
+    int mes = stoi(data.substr(3,2));
+    int ano = stoi(data.substr(6,4));
     
+    if (dia == 31 && (mes == 2 || mes == 4 || mes == 6 || mes == 9 || mes == 11)) {
+        cout << "Data invalida" << endl;
+    } else if (dia == 29 && mes == 2 && (ano % 4 != 0)) {
+        cout << "Data invalida" << endl;
+    } else if (dia > 29 && mes == 2) {
+        cout << "Data invalida" << endl;
+    } else {
+        cout << "Data valida" << endl;
+    }
+}
+
+bool verificarDataValida(string data) {
+    if (data.size() != 10 || data[2] != '/' || data[5] != '/') {
+        return false;
+    }
+    int dia = stoi(data.substr(0,2));
+    int mes = stoi(data.substr(3,2));
+    int ano = stoi(data.substr(6,4));
+    if (dia == 31 && (mes == 2 || mes == 4 || mes == 6 || mes == 9 || mes == 11)) {
+        return false;
+    } else if (dia == 29 && mes == 2 && (ano % 4 != 0)) {
+        return false;
+    } else if (dia > 29 && mes == 2) {
+        return false;
+    } else {
+        return true;
+    }
 }
