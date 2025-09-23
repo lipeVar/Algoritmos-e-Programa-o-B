87e17dd99bcd1c43c3c11059db31caac2dc1ec2a
lipeVar <167947564+lipeVar@users.noreply.github.com>
2025-09-23 00:12:33 +0000

    começo atividade em aula

---
+++ arquivos/revisaoFuncaoArquivo2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+#include "modulo.h"
+
+int main() {
+    
+
+    return 1;
+}
